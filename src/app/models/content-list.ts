619b2b7c882ff6b34c640707c7b36f5ffc60e15e
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2023-01-22 11:06:01 -0500

    Modified class ContentList * added private array of IContent called _items * added constructor which initializes _items to an empty array * get contentList method which returns _items * addContent takes in an IContent object and appends it to _items * listLength method returns _items length

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,21 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+    private _items: IContent[];
+    
+    constructor(){
+        this._items = [];
+    }
+
+    get contentList(): IContent[]{
+        return this._items;
+    }
+
+    addContent(content: IContent){
+        this._items.push(content);
+    }
+
+    listLength(){
+        return this._items.length;
+    }
 }
