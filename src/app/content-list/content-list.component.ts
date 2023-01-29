fe6c966eaafe3c8e7046098c925a3549eccfdc03
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2023-01-29 12:09:43 -0500

    added an array of IContent items to the content-list component

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
 
 @Component({
   selector: 'app-content-list',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
+  private _contentArray: IContent[];
 
+   constructor(){
+    this._contentArray = [{
+      id: 1,
+      name: "Bulbasaur",
+      description: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+      author: "Ben",
+      imgSrc: "assets/imgs/001Bulbasaur.png", 
+      type: "Grass/Poison"
+    },
+    {
+      id: 4,
+      name: "Charmander",
+      description: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+      author: "Ben",
+      imgSrc: "assets/imgs/004Charmander.png", 
+      type: "Fire"
+    },
+    {
+      id: 7,
+      name: "Squirtle",
+      description: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+      author: "Ben",
+      imgSrc: "assets/imgs/007Squirtle.png", 
+      type: "Water"
+    },
+    {
+      id: 25,
+      name: "Pikachu",
+      description: "When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.",
+      author: "Ben",
+      imgSrc: "assets/imgs/025Pikachu.png", 
+      type: "Electric"
+    }];
+  }
 }
