ad872942cfe4ed7a6fef2c99e0728ea00a32cd0e
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2023-02-11 13:11:04 -0400

    routing added to app, now able to direct users to /content by default displaying a list or content/id to display the ItemDetailsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 const routes: Routes = [
-
+  {
+    path: "content",
+    component: ContentListComponent
+  },
+  {
+    path: "",
+    redirectTo: "/content",
+    pathMatch: "full"
+  },
+  {
+  path: "content/:id",
+  component: ItemDetailsComponent
+  }
 ];
 
 @NgModule({
