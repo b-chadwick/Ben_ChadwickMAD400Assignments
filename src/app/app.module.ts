7f680e7424d49aa0cb7027a33eb42cd7015490b6
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2023-02-11 12:41:41 -0400

    version 0.4.0 added new item-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentListItemComponent
+    ContentListItemComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule
