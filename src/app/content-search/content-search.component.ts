1cf231af1504c18126693ffa33a5dfd4040f084f
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2023-02-11 13:33:07 -0400

    fixed routing

--- src/app/content-search/content-search.component.ts
+++ src/app/content-search/content-search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-content-search',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-search.component.scss']
 })
 export class ContentSearchComponent {
+  contentItem: IContent | undefined;
 
+  constructor(private pokemonService: PokemonService){
+    this.contentItem = undefined;
+  }
+
+  ngOnInit(){
+    this.pokemonService.getItem(4)?.subscribe(content=> this.contentItem = content);
+  }
 }
