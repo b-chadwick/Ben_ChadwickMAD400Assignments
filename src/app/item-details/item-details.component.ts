7324e50d18b7d01b2eb9142a8785785f03f2630a
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2023-02-11 15:10:25 -0400

    item retrieved in contentdetail and used to fill information on the screen, link added to this screen to navigate home

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IContent } from '../models/icontent';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-item-details',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-details.component.scss']
 })
 export class ItemDetailsComponent {
+  id?: number;
+  contentItem?: IContent;
+  constructor(private route: ActivatedRoute, private pokemonService: PokemonService){
 
+  }
+
+  ngOnInit(){
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pokemonService.getItem(this.id).subscribe(content =>{this.contentItem = content});
+  }
 }
